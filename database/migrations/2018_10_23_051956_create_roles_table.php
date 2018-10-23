004033a89f2dba1ebff6a4ba7ca9c57f78c5607e
Kilua88 <perezmiguel0788@gmail.com>
2018-10-23 02:46:44 -0300

    se creo los accesos para usuarios/gym y para el administrador del servicio

--- database/migrations/2018_10_23_051956_create_roles_table.php
+++ database/migrations/2018_10_23_051956_create_roles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateInstructorTable extends Migration
+class CreateRolesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,11 @@ class CreateInstructorTable extends Migration
      */
     public function up()
     {
-        Schema::table('instructor', function (Blueprint $table) {
-            //
+        Schema::create('roles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('description');
+            $table->timestamps();
         });
     }
 
@@ -25,8 +28,6 @@ class CreateInstructorTable extends Migration
      */
     public function down()
     {
-        Schema::table('instructor', function (Blueprint $table) {
-            //
-        });
+        Schema::dropIfExists('roles');
     }
 }
--- database/migrations/2018_10_23_052211_create_role_user_table.php
+++ database/migrations/2018_10_23_052211_create_role_user_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateActividadesTable extends Migration
+class CreateRoleUserTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,11 @@ class CreateActividadesTable extends Migration
      */
     public function up()
     {
-        Schema::table('actividades', function (Blueprint $table) {
-            //
+        Schema::create('role_user', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('role_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->timestamps();
         });
     }
 
@@ -25,8 +28,6 @@ class CreateActividadesTable extends Migration
      */
     public function down()
     {
-        Schema::table('actividades', function (Blueprint $table) {
-            //
-        });
+        Schema::dropIfExists('role_user');
     }
 }
